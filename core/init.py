deed5be7d3ea56ef49701a481f23215766bb483a
ye <15888520997@sina.cn>
2024-01-13 11:21:54 +0800

    v1

--- core/init.py
+++ core/init.py
@@ -1,17 +1,17 @@
 from model.sqlite.create import create_tables, create_init_user
-
-
-def check_conn_mongo():
-    pass
+from core.svclog import svc_log_info
 
 
 class Inits(object):
 
-    def init_mongo(self):
-        pass
-
     def init_sqlite(self):
+        svc_log_info("init sqlite db ....")
         create_tables()
+        svc_log_info("init sqlite db ok")
 
     def init_user_and_role(self):
+        svc_log_info("init user and role ....")
         create_init_user()
+        svc_log_info("init user and role ok")
+
+
