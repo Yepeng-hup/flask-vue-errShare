9ed2ea951f8c1732d8c9962030729214e891301c
ye <15888520997@sina.cn>
2024-01-15 14:38:20 +0800

    v1

--- core/svclog.py
+++ core/svclog.py
@@ -3,19 +3,25 @@ from datetime import datetime
 current_time = datetime.now()
 
 
+def log_write_file(log):
+    with open('log/errshare.log', 'a') as file:
+        file.write(f'{log}\n')
+    return
+
+
 def svc_log_info(log_str):
     log = "errShare *** [INFO] "+"["+current_time.strftime("%Y-%m-%d %H:%M:%S")+"]"+" --> "+log_str
-    print(log)
+    log_write_file(log)
     return
 
 
 def svc_log_err(log_str):
     log = "errShare *** [ERROR] "+"["+current_time.strftime("%Y-%m-%d %H:%M:%S")+"]"+" --> "+log_str
-    print(log)
+    log_write_file(log)
     return
 
 
 def svc_log_warn(log_str):
     log = "errShare *** [WARN] "+"["+current_time.strftime("%Y-%m-%d %H:%M:%S")+"]"+" --> "+log_str
-    print(log)
+    log_write_file(log)
     return
